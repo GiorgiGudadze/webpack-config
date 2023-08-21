f43a191a41dc463e9007b9a2f2b57ddcbd1509bc
GiorgiGudadze <gudadzegio317@gmail.com>
2023-08-22 00:38:14 +0400

    Live server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,24 @@
-import { BuildPath } from "./config/build/types/config";
+import { BuildEnv, BuildPath } from "./config/build/types/config";
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
 import path from "path";
 
-const paths: BuildPath = {
-	entry: path.resolve(__dirname, 'src', 'index.ts'),
-	build: path.resolve(__dirname, 'bundle'),
-	html: path.resolve(__dirname, 'public', 'index.html')
-}
+export default (env:BuildEnv) =>{
+	const paths: BuildPath = {
+		entry: path.resolve(__dirname, 'src', 'index.ts'),
+		build: path.resolve(__dirname, 'bundle'),
+		html: path.resolve(__dirname, 'public', 'index.html')
+	}
+	
+	const mode = env.mode || 'development'
+	const isDev = mode === 'development'
+	const port = env.port || 3000
 
-const mode = 'development'
-const isDev = mode === 'development'
+	const config = buildWebpackConfig({
+		mode,
+		paths,
+		isDev,
+		port
+	})
 
-const config = buildWebpackConfig({
-	mode: 'development',
-	paths,
-	isDev
-})
-
-export default config;
+	return config;
+} 
